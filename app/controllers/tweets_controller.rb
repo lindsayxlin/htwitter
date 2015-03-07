bcd8a21643cfe7fabeb184b97679cbf51009da82
Lindsay <xlin@jd15.law.harvard.edu>
2015-03-07 16:05:43 -0500

    index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,15 +6,27 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @hello = "hello"
     @tweet = Tweet.new(tweet_params)
-    @tweet.save
-    render 'new'
+
+     if @tweet.save
+    flash[:success] = "You have created a tweet"
+
+    #make sure flash appears by going to application.html
+       redirect_to new_tweet_path
+       #render vs reditrect
+    end
+
 
     #goes back to new -- go back to original website
     #saves tweet to the database
 # ruby automatically adds a return to each fcn
   end
+
+  def index
+@tweets = Tweet.all
+
+  end
+
 
   def tweet_params
     params.require(:tweet).permit(:content)
