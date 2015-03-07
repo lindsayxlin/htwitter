83a3f69ae36cfcd5e5c89d36f5b187773e4fa395
Lindsay <xlin@jd15.law.harvard.edu>
2015-03-07 14:11:12 -0500

    added tweet creation mech

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,22 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
     #make new instace of tweet object and call it @tweet
   end
+
+  def create
+    @hello = "hello"
+    @tweet = Tweet.new(tweet_params)
+    @tweet.save
+    render 'new'
+    #goes back to new
+    #saves tweet to the database
+# ruby automatically adds a return to each fcn
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+    #only thing we are going to permit is the CONTENT
+  end
+
+#the two @tweets have no idea about each other!
+
 end
